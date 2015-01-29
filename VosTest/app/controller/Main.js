24981c0c4f54b76c6d8ed413d07657cc72f858af
Thomas Schalldach <register@thomas-schalldach.de>
2015-01-29 16:25:10 +0100

    Beep

--- VosTest/app/controller/Main.js
+++ VosTest/app/controller/Main.js
@@ -63,9 +63,9 @@ Ext.define('VosNavigator.controller.Main', {
 
     startButtonAction: function(button, e, eOpts) {
 
-        naviagtor.notification.beep(2);
         this.getMainView().hide();
         this.getFahrplanerView().show({type:"slide",direction:"left"});
+        navigator.notification.beep();
     },
 
     gameButton: function(button, e, eOpts) {
