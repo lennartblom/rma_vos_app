b53f5e956f9e4e755b806e4cb35af6b04b8e27e3
Lennart Blom <mail@lennart-blom.de>
2015-02-03 15:45:17 +0100

    Wecker.js

--- VosTest/app/controller/Wecker.js
+++ VosTest/app/controller/Wecker.js
@@ -67,6 +67,7 @@ Ext.define('VosNavigator.controller.Wecker', {
         this.weckerIsOn=newValue;
         console.log("Toggle Field value: "+newValue);
         this.getGeo(newValue);
+
     },
 
     onSelectfieldChange: function(selectfield, newValue, oldValue, eOpts) {
@@ -84,6 +85,7 @@ Ext.define('VosNavigator.controller.Wecker', {
         this.lng = 0.0;
         this.trackingId = 0;
         this.activeInterval=0;
+        this.bgGeo = null;
     },
 
     wecken: function() {
@@ -102,34 +104,104 @@ Ext.define('VosNavigator.controller.Wecker', {
         var pace = this.getApplication().getController('Settings').sliderPace;
 
         if(isTracking){
+            this.setupBackgroundGeo();
             console.log("device is tracking");
             this.setupGeoTimer(pace*1000);
+            bgGeo.start();
 
         }else{
             clearInterval(this.activeInterval);
+            bgGeo.stop();
             console.log("pace disabled");
         }
     },
 
     setupGeoTimer: function(interval) {
-        this.activeInterval = setInterval(saveGeo,interval);
+        this.activeInterval = setInterval(this.saveGeo,interval);
     },
 
     saveGeo: function() {
         console.log("saveGeo wurde aufgerufen.");
         var geoObject = navigator.geolocation.getCurrentPosition(
-                function(){
-                    console.log(position.coords.latitude);
-                    console.log(position.coords.longitude);
-
-                },
-                function(){
-                    console.log("error while paceing");
-                },
-                {
-                    enableHighAccuracy: true
-                });
+                geoCallback(position),
+            geoError, {enableHighAccuracy:true});
         console.log(geoObject);
+    },
+
+    setupBackgroundGeo: function() {
+
+                // Your app must execute AT LEAST ONE call for the current position via standard Cordova geolocation,
+                //  in order to prompt the user for Location permission.
+                window.navigator.geolocation.getCurrentPosition(function(location) {
+                    console.log('Location from Phonegap');
+                });
+
+                this.bgGeo = window.plugins.backgroundGeoLocation;
+
+                /**
+                * This would be your own callback for Ajax-requests after POSTing background geolocation to your server.
+                */
+                var yourAjaxCallback = function(response) {
+                    ////
+                    // IMPORTANT:  You must execute the #finish method here to inform the native plugin that you're finished,
+                    //  and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+                    // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+                    //
+                    //
+                    bgGeo.finish();
+                };
+
+                /**
+                * This callback will be executed every time a geolocation is recorded in the background.
+                */
+                var callbackFn = function(location) {
+                    console.log('[js] BackgroundGeoLocation callback:  ' + location.latitudue + ',' + location.longitude);
+                    // Do your HTTP request here to POST location to your server.
+                    //
+                    //
+                    yourAjaxCallback.call(this);
+                };
+
+                var failureFn = function(error) {
+                    console.log('BackgroundGeoLocation error');
+                };
+
+                // BackgroundGeoLocation is highly configurable.
+                this.bgGeo.configure(callbackFn, failureFn, {
+                    url: 'http://only.for.android.com/update_location.json', // <-- only required for Android; ios allows javascript callbacks for your http
+                    params: {                                               // HTTP POST params sent to your server when persisting locations.
+                        auth_token: 'user_secret_auth_token',
+                        foo: 'bar'
+                    },
+                    headers: {
+                        'X-Foo': 'bar'
+                    },
+                    desiredAccuracy: 10,
+                    stationaryRadius: 20,
+                    distanceFilter: 30,
+                    notificationTitle: 'Background tracking',   // <-- android only, customize the title of the notification
+                    notificationText: 'ENABLED',                // <-- android only, customize the text of the notification
+                    activityType: "AutomotiveNavigation",       // <-- iOS-only
+                    debug: true     // <-- enable this hear sounds for background-geolocation life-cycle.
+                });
+
+                // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+                this.bgGeo.start();
+
+                // If you wish to turn OFF background-tracking, call the #stop method.
+                // bgGeo.stop()
+
+    },
+
+    geoCallback: function(position) {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+        this.lat=position.coords.latitude;
+        this.lng=position.coords.longitude;
+    },
+
+    geoError: function() {
+        console.log("error while paceing");
     }
 
 });
